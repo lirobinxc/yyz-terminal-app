f8e1cc57719ca3dde3a8750fcd4698eaa0ec1965
Robin Li <lirobinxc@gmail.com>
2022-11-25 22:12:05 -0500

    feat: added SIDs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Freqs from './data/cjs-freq/Freqs';
 import { useState } from 'react';
 
-import './App.scss';
 import Airports from './data/airports/Airports';
+import Sids, { IRunway } from './data/sids/Sids';
+
+import './App.scss';
 
 const Topic = {
-  Freqs: 'Freqs',
   Home: 'Home',
   Airports: 'Airports',
+  Freqs: 'Freqs',
+  SIDs: IRunway,
 };
 
 function App() {
@@ -23,7 +26,7 @@ function App() {
         <h1>
           ✈️
           <br />
-          YYZ Terminal App 1.5
+          YYZ Terminal App 1.7
         </h1>
         <h2>Select a topic. </h2>
       </section>
@@ -35,6 +38,24 @@ function App() {
           <li className="topicRow" onClick={() => setTopic(Topic.Airports)}>
             Airports
           </li>
+          <li
+            className="topicRow"
+            onClick={() => setTopic(Topic.SIDs['05,06LR'])}
+          >
+            SIDs - 05/06LR
+          </li>
+          <li className="topicRow" onClick={() => setTopic(Topic.SIDs['15LR'])}>
+            SIDs - 15LR
+          </li>
+          <li
+            className="topicRow"
+            onClick={() => setTopic(Topic.SIDs['23,24LR'])}
+          >
+            SIDs - 23/24LR
+          </li>
+          <li className="topicRow" onClick={() => setTopic(Topic.SIDs['33LR'])}>
+            SIDs - 33LR
+          </li>
         </ol>
       </section>
     </div>
@@ -45,6 +66,18 @@ function App() {
       {topic === Topic.Home && Homepage}
       {topic === Topic.Freqs && <Freqs />}
       {topic === Topic.Airports && <Airports />}
+      {topic === Topic.SIDs['05,06LR'] && (
+        <Sids runwayConfig={Topic.SIDs['05,06LR']} />
+      )}
+      {topic === Topic.SIDs['15LR'] && (
+        <Sids runwayConfig={Topic.SIDs['15LR']} />
+      )}
+      {topic === Topic.SIDs['23,24LR'] && (
+        <Sids runwayConfig={Topic.SIDs['23,24LR']} />
+      )}
+      {topic === Topic.SIDs['33LR'] && (
+        <Sids runwayConfig={Topic.SIDs['33LR']} />
+      )}
       <div className="footerSection">
         {topic !== Topic.Home && (
           <button onClick={returnHome} className="homeButton">
---
+++ src/data/sids/Sids.jsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from 'react';
+import { sidsData } from './sidsData.js';
+import clsx from 'clsx';
+
+import '../../App.scss';
+
+export const IRunway = {
+  '05,06LR': '05,06LR',
+  '15LR': '15LR',
+  '23,24LR': '23,24LR',
+  '33LR': '33LR',
+};
+
+export default function Sids({ runwayConfig }) {
+  const [randomList, setRandomList] = useState(sidsData['05,06LR']);
+  const [showAnswer, setShowAnswer] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [startTime, setStartTime] = useState(Date.now());
+  const [endTime, setEndTime] = useState(Date.now());
+  const [isClickEnabled, setIsClickEnabled] = useState(true);
+
+  function randomize(list) {
+    const newList = list
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    newList.push({ isDone: true });
+
+    return newList;
+  }
+
+  useEffect(() => {
+    setRandomList(randomize(sidsData[runwayConfig]));
+  }, [runwayConfig]);
+
+  function handleShowAnswer() {
+    const isFirstItem = index === 0;
+    if (isFirstItem) {
+      setStartTime(Date.now());
+    }
+
+    setShowAnswer(false);
+  }
+
+  function reset() {
+    setIndex(0);
+    setRandomList(randomize(sidsData));
+    setStartTime(Date.now());
+    setShowAnswer(true);
+  }
+
+  function handleNextQuestion() {
+    if (!isClickEnabled) return;
+
+    const isLastItem = index === randomList.length - 1;
+    if (isLastItem) {
+      setIsClickEnabled(false);
+      setEndTime(Date.now());
+      setTimeout(() => {
+        setIsClickEnabled(true);
+        reset();
+      }, 4000);
+    } else {
+      setIndex(index + 1);
+    }
+    setShowAnswer(true);
+  }
+
+  function displayTimer() {
+    const timeElapsed = endTime - startTime;
+
+    if (timeElapsed <= 0) return 'Click to show score';
+
+    return `${(timeElapsed / 1000).toFixed(1)} seconds`;
+  }
+
+  function DisplayList() {
+    if (!randomList) return;
+    const nextItem = randomList[index];
+
+    const containerClass = clsx('cardContainer', 'Sids', {
+      disableClick: !isClickEnabled,
+    });
+
+    if (showAnswer && !nextItem.isDone) {
+      return (
+        <div className={containerClass} onClick={handleShowAnswer}>
+          <div className="data1">{nextItem.Name}</div>
+          <div className="data3"></div>
+          <div className="data2">___</div>
+          <div className="data4"></div>
+        </div>
+      );
+    } else if (nextItem.isDone) {
+      return (
+        <div className={containerClass} onClick={handleNextQuestion}>
+          <div className="data1">Done!</div>
+          <div className="data2">{displayTimer()}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={containerClass} onClick={handleNextQuestion}>
+          <div className="data1">{nextItem.Name}</div>
+          <div className="data3">{nextItem['Aircraft type']}</div>
+          <div className="data2">
+            {nextItem['First WP']} - {nextItem['Route WP']} -{' '}
+            {nextItem['Final WP']}
+          </div>
+          <div className="data4">
+            {nextItem['Further WP'] && `- ${nextItem['Further WP']}`}
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <section className="TopicPage">
+      <div className="topicTitleRow">
+        <div>SIDs - {runwayConfig}</div>
+        <div>
+          Remaining: {randomList ? randomList.length - index - 1 : '0'}/
+          {sidsData[runwayConfig].length}
+        </div>
+      </div>
+      {DisplayList()}
+      <button className="resetButton" onClick={reset}>
+        RESET
+      </button>
+    </section>
+  );
+}
